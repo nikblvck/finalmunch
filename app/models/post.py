04cac9ff3e118f796172e699e0c07b8f5b083b59
nik <nikblvck@icloud.com>
2022-06-02 15:43:46 -0700

    first commit

---
+++ app/models/post.py
@@ -0,0 +1,31 @@
+from .db import db
+
+class Post(db.Model):
+  __tablename__ = 'posts'
+
+  id = db.Column(db.Integer, primary_key=True)
+  caption = db.Column(db.Text, nullable=True)
+  category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+  updated_at = db.Column(db.DateTime, nullable=False, default=db.func.now(), onupdate=db.func.now())
+
+  @property
+  def post_summary(self):
+    return self.caption[:100]
+
+  def to_dict(self):
+    return {
+      'id': self.id,
+      'caption': self.caption,
+      'category_id': self.category_id,
+      'user_id': self.user_id,
+      'created_at': self.created_at,
+      'updated_at': self.updated_at
+    }
+
+  def __repr__(self):
+    return '<Post %r>' % self.id
+
+  def __str__(self):
+    return self.__repr__()
