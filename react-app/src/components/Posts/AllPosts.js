2df680d1d14c1e1c36bc43599420e02958fd4be5
nik <nikblvck@icloud.com>
2022-06-04 17:24:52 -0700

    updated post routes, initialized components

---
+++ react-app/src/components/Posts/AllPosts.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function AllPosts() {
+
+  return (
+    <>
+      <h1>All Posts</h1>
+    </>
+  )
+
+}
+
+
+export default AllPosts;
---
+++ react-app/src/components/Posts/NewPost.js
@@ -0,0 +1,11 @@
+import {useEffect, useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+
+function NewPost(){
+
+
+  return (
+    <>
+    </>
+  );
+}
